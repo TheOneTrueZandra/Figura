5ca0c28a42cd091bb53302ae0116390ade8f1989
Hibi <hibi@tuta.io>
2021-04-22 07:02:41 -0300

    Prefer MutableText over LiteralText  2: Electric Boogaloo
    sleepyhead hibi forgot to save before committing

--- src/main/java/net/blancworks/figura/mixin/PlayerListHudMixin.java
+++ src/main/java/net/blancworks/figura/mixin/PlayerListHudMixin.java
@@ -1,14 +1,11 @@
 package net.blancworks.figura.mixin;
 
 import net.blancworks.figura.Config;
-import net.blancworks.figura.FiguraMod;
 import net.blancworks.figura.PlayerDataManager;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.text.BaseText;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +19,7 @@ public class PlayerListHudMixin {
         if (!Config.nameTagMark.value)
             return;
         
-        BaseText text = (BaseText) cir.getReturnValue();
+        MutableText text = (MutableText) cir.getReturnValue();
         text.append(PlayerDataManager.getDataForPlayer(entry.getProfile().getId()).getNameDecorations());
         cir.setReturnValue(text);
     }
